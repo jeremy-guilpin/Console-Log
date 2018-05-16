9b50c61ec0ab7c3a08e0cb8bd1c3147c63fca63c
Jérémy Guilpin <jeremy-guilpin@users.noreply.github.com>
2018-05-16 11:57:22 +0200

    Update console-log.js

--- console-log.js
+++ console-log.js
@@ -3,11 +3,11 @@ var DEBUG = true;
 
 // Fonction custom de console.log
 function _log(a, b, c, d) {
-	var args        = (arguments.length === 1 ? arguments[0] : Array.apply(null, arguments)),
-		e       = new Error(),
-		_line   = e.stack,
-		_lines  = _line.split("\n"),
-	    	_trace  = _lines[2].substring(_lines[2].indexOf('('), _lines[2].lastIndexOf(')') + 1);
+	var _args      = (arguments.length === 1 ? arguments[0] : Array.apply(null, arguments)),
+		_e     = new Error(),
+		_line  = _e.stack,
+		_lines = _line.split("\n"),
+		_trace = _lines[2].substring(_lines[2].indexOf('('), _lines[2].lastIndexOf(')') + 1);
 
-	return (DEBUG === true) ? console.log('CONSOLE =>', args, _trace) : null;
+	return (DEBUG === true) ? console.log('CONSOLE =>', _args, _trace) : null;
 }
