715683d5f67f201d5f0dbf25bce7b18abea6e7e8
Jérémy Guilpin <jeremy-guilpin@users.noreply.github.com>
2018-05-17 15:14:30 +0200

    Update console-log.js

--- console-log.js
+++ console-log.js
@@ -1,6 +1,9 @@
 // Variable Global de debug (Elle peut être contenu dans un objet)
 var DEBUG = true;
 
+// On met à jour un numéro de version pour le cache navigateur
+console.log('-- Version cache form control 1.' + Math.floor(Math.random() * Math.floor(1000)) + ' --');
+
 // Fonction custom de console.log
 function _log(a, b, c, d) {
 	var _args      = (arguments.length === 1 ? arguments[0] : Array.apply(null, arguments)),
