eaf33734140befb1ba3d83ab1bd08b55d44b36aa
Jérémy Guilpin <jeremy-guilpin@users.noreply.github.com>
2018-06-05 09:29:04 +0200

    Update console-log.js
    control sup pour IE

--- console-log.js
+++ console-log.js
@@ -6,11 +6,11 @@ console.log('-- Version cache form control 1.' + Math.floor(Math.random() * Math
 
 // Fonction custom de console.log
 function _log(a, b, c, d) {
-	var _args      = (arguments.length === 1 ? arguments[0] : Array.apply(null, arguments)),
-		_e     = new Error(),
-		_line  = _e.stack,
-		_lines = _line.split("\n"),
-		_trace = _lines[2].substring(_lines[2].indexOf('('), _lines[2].lastIndexOf(')') + 1);
+	var _args = (arguments.length === 1 ? arguments[0] : Array.apply(null, arguments)),
+		_e = new Error(),
+		_line = _e.stack,
+		_lines = (_line != null) ? _line.split("\n") : null,
+		_trace = (_lines != null) ? _lines[2].substring(_lines[2].indexOf('('), _lines[2].lastIndexOf(')') + 1) : null;
 
-	return (DEBUG === true) ? console.log('CONSOLE =>', _args, _trace) : null;
+	return (DEBUG === true && _trace != null) ? console.log('CONSOLE =>', _args, _trace) : null;
 }
